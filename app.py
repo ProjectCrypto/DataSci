12dd433c7bd773f4a3562a6c01d3f5963dce39b6
eentr928 <eentr928@live.kutztown.edu>
2017-11-07 20:52:19 -0500

    Made a new site, can't help it

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+from content_manager import Content
+
+TOPIC_DICT = Content()
+
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+    return render_template("index.html", TOPIC_DICT = TOPIC_DICT )
+
+@app.route("/posting/<post_url>")
+def posting(post_url):
+    return render_template("post.html",post_url = post_url)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
---
+++ content_manager.py
@@ -0,0 +1,20 @@
+def Content():
+    TOPIC_DICT = [
+        {'title':'Welcome',
+         'link':'website-example.html',
+         'summary':'Example of a posting to show how awesome this site will be.',
+         'image':'analysis1.png'
+        },
+        {'title':'Another Project!',
+         'link':'posts/website-example.html',
+         'summary':'With a different description...',
+         'image':'analysis2.jpg'
+        },
+        {'title':'Another Project Again!!',
+         'link':'posts/website-example.html',
+         'summary':'With a different description...',
+         'image':'analysis3.png'
+        }
+    ]
+    
+    return TOPIC_DICT
