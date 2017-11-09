fb73748712115a81be872735ba43691be3d4eee6
eentr928 <eentr928@live.kutztown.edu>
2017-11-08 22:27:26 -0500

    Just Pushing to start someting different

--- content_manager.py
+++ content_manager.py
@@ -1,16 +1,22 @@
 def Content():
     TOPIC_DICT = [
+        {'title':'Sentiment Analysis of Crypto Currency',
+         'data':'11/8/2017',
+         'authors':['Eric Entrup'],
+         'link':'post-sentiment-crypto.html',
+         'summary':'With a different description...',
+         'image':'analysis2.jpg'
+        },
         {'title':'Examlple Post',
+         'data':'11/8/2017',
+         'authors':['Eric Entrup'],
          'link':'example.html',
          'summary':'This will serve to help mitigate the creation of new posts and how each post can be used.',
          'image':'example.jpg'
         },
-        {'title':'Another Project!',
-         'link':'website-example.html',
-         'summary':'With a different description...',
-         'image':'analysis2.jpg'
-        },
         {'title':'Another Project Again!!',
+         'data':'11/8/2017',
+         'authors':['Eric Entrup'],
          'link':'website-example.html',
          'summary':'With a different description...',
          'image':'analysis3.png'
